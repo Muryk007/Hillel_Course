c03bbc42c8c671aec9f06736a1be8512506f0bb3
Muryk007 <59446950+Muryk007@users.noreply.github.com>
2025-07-10 01:28:05 +0300

    Update homework_06_4.py

--- lesson_06_4/homework_06_4.py
+++ lesson_06_4/homework_06_4.py
@@ -1,8 +1,13 @@
 my_lst =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 10, 11, 12, 13, 14, 15]
 duplicates = []
 
+# for item in my_lst:
+#         if my_lst.count(item) > 1:
+#                 duplicates.append(item)
+
 for item in my_lst:
-        if my_lst.count(item) > 1:
-                duplicates.append(item)
+        if item % 2 == 0:
+            duplicates.append(item)
+        print(duplicates)
 
 print("Сума усіх парних чисел =", sum(duplicates))
