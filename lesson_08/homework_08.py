de6708bdeab8b5b49c1751fcc10a20d5eecf2e56
Muryk007 <muryk007@gmail.com>
2025-07-16 23:22:27 +0300

    Homework_08

---
+++ lesson_08/homework_08.py
@@ -0,0 +1,8 @@
+massive = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+
+for item in massive:
+    try:
+        numbers = [int(num.strip()) for num in item.split(",")]
+        print(f'сума: {sum(numbers)}')
+    except ValueError:
+        print(f' "Не вдалось опрацювати елементи: {item}"')
