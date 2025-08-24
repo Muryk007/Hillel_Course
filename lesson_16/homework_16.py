184ecac8e89607fba71cfe9ec2046193cabf0ec2
Muryk007 <59446950+Muryk007@users.noreply.github.com>
2025-08-24 22:13:08 +0300

    Update homework_16.py

--- lesson_16/homework_16.py
+++ lesson_16/homework_16.py
@@ -1,3 +1,7 @@
+import math
+from abc import abstractmethod, ABC
+
+
 # Завдання 1
 
 # # Створіть клас Employee, який має атрибути name та salary. Далі створіть два класи, Manager та Developer,
@@ -9,14 +13,6 @@
 # team_size, який вказує на кількість розробників у команді, якою керує керівник.
 #
 # Напишіть тест, який перевіряє наявність атрибутів з Manager та Developer у класі TeamLead
-#
-# Завдання 2
-#
-# Створіть абстрактний клас "Фігура" з абстрактними методами для отримання площі та периметру. Наслідуйте від нього
-# декілька (> 2) інших фігур, та реалізуйте математично вірні для них методи для площі та периметру. Властивості по
-# типу “довжина сторони” й т.д. повинні бути приватними, та ініціалізуватись через конструктор. Створіть Декілька
-# різних об’єктів фігур, та у циклі порахуйте та виведіть в консоль площу та периметр кожної.
-
 
 class Employee:
     def __init__(self, name, salary, **kwargs)   :
@@ -52,3 +48,72 @@ class TeamLead(Manager, Developer):
 # print(lead.department)
 # print(lead.programming_language)
 # print(lead.team_size)
+
+# Завдання 2
+#
+# Створіть абстрактний клас "Фігура" з абстрактними методами для отримання площі та периметру. Наслідуйте від нього
+# декілька (> 2) інших фігур, та реалізуйте математично вірні для них методи для площі та периметру. Властивості по
+# типу “довжина сторони” й т.д. повинні бути приватними, та ініціалізуватись через конструктор. Створіть Декілька
+# різних об’єктів фігур, та у циклі порахуйте та виведіть в консоль площу та периметр кожної.
+
+class Figure(ABC):
+    @abstractmethod
+    def figure_square(self):
+        pass
+
+    @abstractmethod
+    def figure_perimeter(self):
+        pass
+
+class Quadrant(Figure):
+    def __init__(self):
+        self.__side_quad_a = 2
+        self.__side_quad_b = 2
+
+    def figure_square(self):
+        square = self.__side_quad_a * self.__side_quad_b
+        return square
+
+    def figure_perimeter(self):
+        perimeter = (self.__side_quad_a + self.__side_quad_b) * 2
+        return perimeter
+
+class Round(Figure):
+    def __init__(self):
+        self.__pi = 3.14
+        self.__radius = 8
+
+    def figure_square(self):
+        square = pow((self.__radius), 2) * self.__pi
+        return square
+
+    def figure_perimeter(self):
+        perimeter = self.__pi * self.__radius*2
+        return perimeter
+
+class Trapezoid(Figure):
+    def __init__(self):
+        self.__side_thrap_a = 4
+        self.__side_thrap_b = 8
+        self.__high_thrap = 10
+
+    def figure_square(self):
+        square = (self.__side_thrap_a + self.__side_thrap_b) * self.__high_thrap / 2
+        return square
+
+    def figure_perimeter(self):
+        side_c_d = math.sqrt(pow((self.__side_thrap_b - self.__side_thrap_a), 2) + pow(self.__high_thrap, 2))
+        perimeter = self.__side_thrap_a + self.__side_thrap_b + side_c_d * 2
+        return perimeter
+
+box = Quadrant()
+circle = Round()
+thrap = Trapezoid()
+
+shapes = [box, circle, thrap]
+
+for shape in shapes:
+    print(f'{shape.__class__.__name__}:')
+    print(f'Площа: {shape.figure_square()}')
+    print(f'Периметр: {shape.figure_perimeter()}')
+    print('-' * 50)
