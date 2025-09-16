5165e6fac9e9e799f1a1978ecfb48234a2088f4c
oleh <muryk007@gmail.com>
2025-09-16 17:57:47 +0300

    Homework_22

---
+++ core/db/orm_lesson/tables/students.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Базовий клас для визначення моделей даних
+Base = declarative_base()
+
+# Визначення моделі даних (таблиці) за допомогою класу
+class User(Base):
+    __tablename__ = 'users_orm'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    age = Column(Integer)
